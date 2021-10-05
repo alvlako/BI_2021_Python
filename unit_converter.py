d5f1a4bd4c72958d565b838a8cb8ccb0ccc0ec6f
alvlako <56854264+alvlako@users.noreply.github.com>
2021-10-05 19:49:54 +0200

    editing as in PEP8

--- unit_converter.py
+++ unit_converter.py
@@ -1,42 +1,42 @@
 #converter and making conflicts
-#type smth just for conflict
-units=['g', 'm','m','s','s']
-units_alternative=['gram', 'metre','minute','sec','sec']
-units_pairs=['stone','km','h','min','h']
-units_pairs_alt=['stone','kilometre','hour','minute','hour']
+units = ['g', 'm', 'm', 's', 's']
+units_alternative = ['gram', 'metre', 'minute', 'sec', 'sec']
+units_pairs = ['stone', 'km', 'h', 'min', 'h']
+units_pairs_alt = ['stone', 'kilometre', 'hour', 'minute', 'hour']
 convert_method=[6350.29318, 1000, 60, 60, 3600]
 while True:
     print("Available units: g,stone,m,km,min,s,m,h. Print exit to exit")
-    user_exit=[]
+    user_exit = []
     try:
         user_exit.append(input())
-        if user_exit[0]=="exit" or user_exit[0]=="Exit":
+        if user_exit[0] == "exit" or user_exit[0] == "Exit":
             break
     except EOFError:
         continue
     print("Type 1st unit name (convert from)")
-    unit_1=str(input())
+    unit_1 = str(input())
     print("Type how much units 1 you have")
     try:
-        unit_number=int(input())
+        unit_number = int(input())
     except ValueError:
         print("You have to type numeric value")
     print("Type 2nd unit name (convert to)")
-    unit_2=str(input())
-    def my_converter (unit_1,unit_2):
+    unit_2 = str(input())
+
+    def my_converter(unit_1, unit_2):
         try:
-            index1=(units+units_alternative+units_pairs+units_pairs_alt).index(unit_1)
-            real_index1=index1%len(units)
-            list_n_1=index1//len(units)
+            index1 = (units+units_alternative+units_pairs+units_pairs_alt).index(unit_1)
+            real_index1 = index1%len(units)
+            list_n_1 = index1//len(units)
         except ValueError:
             print("1st unit is not supported")
         try:
-            index2=(units+units_alternative+units_pairs+units_pairs_alt).index(unit_2)
-            real_index2=index2%len(units)
-            list_n_2=index2//len(units)
+            index2 = (units+units_alternative+units_pairs+units_pairs_alt).index(unit_2)
+            real_index2 = index2%len(units)
+            list_n_2 = index2//len(units)
         except ValueError:
             print("2nd unit is not supported")
-        if real_index1==real_index2 and (list_n_1==list_n_2 or list_n_1==(list_n_2+1) or list_n_2==(list_n_1+1)):
+        if real_index1 == real_index2 and (list_n_1 == list_n_2 or list_n_1 == (list_n_2+1) or list_n_2 == (list_n_1+1)):
             print("You typed the same units! It is not supported!")
         else:
             if list_n_1 > list_n_2:
@@ -44,4 +44,4 @@ while True:
             if list_n_1 < list_n_2:
                 converted_value=convert_method[real_index2]/unit_number
         print(str(converted_value)+" "+str(unit_2))
-    my_converter(unit_1,unit_2)
+    my_converter(unit_1, unit_2)
